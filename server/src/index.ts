1b6a3fb2d20489cdce657094e0d02a6dd0137a8b
kiplagatwillan <willankpl01@gmail.com>
2025-07-29 23:47:58 +0300

    avatar fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 import { PrismaClient } from "@prisma/client";
 
 import authRoutes from "./routes/authRoutes";
@@ -12,10 +13,9 @@ dotenv.config();
 const app = express();
 const prisma = new PrismaClient();
 
-
 app.use(cors({
   origin: [
-    "http://localhost:5173", 
+    "http://localhost:5173",
     "https://tasky-peach.vercel.app"
   ],
   credentials: true
@@ -23,16 +23,15 @@ app.use(cors({
 
 app.use(express.json());
 
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-
-app.use("/api/auth", authRoutes);    
-app.use("/api/user", userRoutes);     
-app.use("/api/tasks", taskRoutes);    
-
+app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/tasks", taskRoutes);
 
 app.get("/api", (req, res) => {
   res.send("Welcome to the API endpoint!");
